6e05dc5ddd55570375d06e62ac2094f92c946957
Cbnns <leo.cabannes@proton.me>
2026-08-06 09:13:56 +0200

    Initial Commit

---
+++ NTS Mini/NTS_MiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  NTS_MiniApp.swift
+//  NTS Mini
+//
+//  Created by Leo Cabannes on 8/6/26.
+//
+
+import SwiftUI
+
+@main
+struct NTS_MiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
